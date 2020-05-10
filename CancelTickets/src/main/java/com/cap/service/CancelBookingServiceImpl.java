db7282cb2088908799894521addb1cd8e880e3b8
DivyaReddy20 <divyachilukuri20@gmail.com>
2020-05-10 19:53:27 +0530

    Added junit

--- CancelTickets/src/main/java/com/cap/service/CancelBookingServiceImpl.java
+++ CancelTickets/src/main/java/com/cap/service/CancelBookingServiceImpl.java
@@ -1,10 +1,8 @@
 package com.cap.service;
 
 import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.cap.dao.CancelBookingDao;
 import com.cap.exceptions.IdNotFoundException;
 
@@ -20,7 +18,6 @@ public class CancelBookingServiceImpl implements CancelBookingService {
 		boolean result=dao.exists(bookingId);
 	
 		if(result) {
-			
 	        dao.delete(bookingId);
 			return "deleted the emp details";
 		}
